df2009379bc06d37ff597c603303448849dcde15
Ravisanker E <rae@adobe.com>
2024-02-28 14:50:08 +0530

    Fix bug in cf

--- blocks/form/functions.js
+++ blocks/form/functions.js
@@ -5,23 +5,6 @@
  * @param {string} lastname in Stringformat
  * @return {string}
  */
-function getFullName(firstname, lastname) {
-  // eslint-disable-next-line no-param-reassign
-  firstname = (firstname == null) ? '' : firstname;
-  // eslint-disable-next-line no-param-reassign
-  lastname = (lastname == null) ? '' : lastname;
-  return firstname.concat(' ').concat(lastname);
-}
-
-// eslint-disable-next-line import/prefer-default-export
-export { getFullName };
-/**
- * Get Full Name
- * @name getFullName Concats first name and last name
- * @param {string} firstname in Stringformat
- * @param {string} lastname in Stringformat
- * @return {string}
- */
  function getFullName(firstname, lastname) {
   // eslint-disable-next-line no-param-reassign
   firstname = (firstname == null) ? '' : firstname;
