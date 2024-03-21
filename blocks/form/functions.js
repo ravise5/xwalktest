767a61e69dcce51ee61ec44cf6c06b350d6ed098
Ravisanker E <rae@adobe.com>
2024-03-21 12:36:12 +0530

    reverting functions.js

--- blocks/form/functions.js
+++ blocks/form/functions.js
@@ -6,11 +6,7 @@
  * @return {string}
  */
 function getFullName(firstname, lastname) {
-  // eslint-disable-next-line no-param-reassign
-  firstname = (firstname == null) ? '' : firstname;
-  // eslint-disable-next-line no-param-reassign
-  lastname = (lastname == null) ? '' : lastname;
-  return firstname.concat(' ').concat(lastname);
+  return `${firstname} ${lastname}`?.trim();
 }
 
 /**
