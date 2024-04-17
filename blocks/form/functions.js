3f0637cd2332d6fa5fdcb10ecc3c3fbc9d5481f6
Ravisanker E <rae@adobe.com>
2024-04-17 11:54:05 +0530

    Update form block

--- blocks/form/functions.js
+++ blocks/form/functions.js
@@ -6,23 +6,14 @@
  * @return {string}
  */
 function getFullName(firstname, lastname) {
-  // eslint-disable-next-line no-param-reassign
-  firstname = (firstname == null) ? '' : firstname;
-  // eslint-disable-next-line no-param-reassign
-  lastname = (lastname == null) ? '' : lastname;
-  return firstname.concat(' ').concat(lastname);
+  return `${firstname} ${lastname}`.trim();
 }
-/**
- * On Wizard Init.
- * @name onWizardInit Runs on initialization of wizard
- */
-function onWizardInit() {}
 
 /**
  * Calculate the number of days between two dates.
  * @param {*} endDate
  * @param {*} startDate
- * @returns returns the number of days between two dates
+ * @returns {number} returns the number of days between two dates
  */
 function days(endDate, startDate) {
   const start = typeof startDate === 'string' ? new Date(startDate) : startDate;
@@ -37,50 +28,5 @@ function days(endDate, startDate) {
   return Math.floor(diffInMs / (1000 * 60 * 60 * 24));
 }
 
-/**
- * Called on form init.
- * @name onFormInit Is called on form init
- */
-function onFormInit() {}
-
-/**
- * Formats telephone input
- * @name formatTelephoneInput Formats telephone input
- * @param {object} field field whose value to be formatted
- * @return {string}
- */
-function formatTelephoneInput(field) {
-  const phoneNumber = field.$value;
-  if (phoneNumber) {
-    const maskedDigits = phoneNumber.substring(0, 7).replace(/\d/g, '*');
-    const lastThreeDigits = phoneNumber.substring(7);
-    return maskedDigits + lastThreeDigits;
-  }
-  return phoneNumber;
-}
-
-/**
- * Formats email input
- * @name formatEmailInput Formats email input
- * @param {object} field field whose value to be formatted
- * @return {string}
- */
-function formatEmailInput(field) {
-  const email = field.$value;
-  let transformedEmail;
-  if (email) {
-    const parts = email.split('@');
-    if (parts[0].length > 1) {
-      transformedEmail = `${parts[0][0] + '*'.repeat(parts[0].length - 1)}@${parts[1]}`;
-    } else {
-      transformedEmail = email;
-    }
-  }
-
-  return transformedEmail;
-}
-
 // eslint-disable-next-line import/prefer-default-export
-export {
-  getFullName, onWizardInit, days, onFormInit, formatTelephoneInput, formatEmailInput,
-};
+export { getFullName, days };
